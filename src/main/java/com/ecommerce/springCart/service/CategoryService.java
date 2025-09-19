69ae51bda223a98444966ab02b1d89bc13b6e0a8
sharvari-09 <jadhavsharvari9113@gmail.com>
2025-09-19 10:19:50 +0530

    "Added Create, Get, Update and Delete Category"

--- src/main/java/com/ecommerce/springCart/service/CategoryService.java
+++ src/main/java/com/ecommerce/springCart/service/CategoryService.java
@@ -8,4 +8,7 @@ public interface CategoryService {
     void createCategory(Category category);
 
 
+    String deleteCategory(Long categoryId);
+
+    Category updateCategory(Category category, Long categoryId);
 }
