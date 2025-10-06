d3d318318e66c759fb6bdfb0b27da62130b267cc
sharvari-09 <jadhavsharvari9113@gmail.com>
2025-10-06 11:33:43 +0530

    Done with orders and payments

---
+++ src/main/java/com/ecommerce/springCart/repositories/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.springCart.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.springCart.model.Payment;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long>{
+
+}
---
+++ src/main/java/com/ecommerce/springCart/service/OrderService.java
@@ -0,0 +1,9 @@
+package com.ecommerce.springCart.service;
+
+import com.ecommerce.springCart.payload.OrderDTO;
+import jakarta.transaction.Transactional;
+
+public interface OrderService {
+    @Transactional
+    OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage);
+}
